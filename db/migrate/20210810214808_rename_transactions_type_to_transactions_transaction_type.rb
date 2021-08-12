1c14d8ef075c563eb441dad645eb77341221b597
Luiz Bosso <lfernando.bosso@gmail.com>
2021-08-12 12:36:48 -0400

    Adds comments about risk

--- db/migrate/20210810214808_rename_transactions_type_to_transactions_transaction_type.rb
+++ db/migrate/20210810214808_rename_transactions_type_to_transactions_transaction_type.rb
@@ -1,5 +1,6 @@
 class RenameTransactionsTypeToTransactionsTransactionType < ActiveRecord::Migration[6.1]
   def change
+    # In a production environment with several records, this might be dangerous
     rename_column :program_manager_transactions, :type, :transaction_type
   end
 end
